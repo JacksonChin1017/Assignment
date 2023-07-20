1607c77dec12c4da3335cb71a7161e00c44d3d2e
weilun <alantang0410@gmail.com>
2023-07-20 15:34:34 +0800

    forloop

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 class Hello{
     public static void main(String[] args) {
          System.out.println("Hello World !");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("gitttt");
+        }
     }
 }
 
